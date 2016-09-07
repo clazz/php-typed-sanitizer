b57cbd37079d2066da098b4c85aa6069e71b2923
Clarence <clarence-pan@foxmail.com>
2016-09-07 13:23:57 +0800

    set seconds optional

--- src/Types/DateTimeType.php
+++ src/Types/DateTimeType.php
@@ -6,7 +6,7 @@ class DateTimeType extends StringType
 {
     protected $type = 'datetime';
 
-    const DEFAULT_PATTERN = '/^[1-2][0-9][0-9][0-9]-[0-1]{0,1}[0-9]-[0-3]{0,1}[0-9] (([0-1][0-9])|(2[0-4])):[0-6][0-9](:[0-6][0-9])$/';
+    const DEFAULT_PATTERN = '/^[1-2][0-9][0-9][0-9]-[0-1]{0,1}[0-9]-[0-3]{0,1}[0-9] (([0-1][0-9])|(2[0-4])):[0-6][0-9](:[0-6][0-9])?$/';
 
     public function __construct($pattern = null)
     {
